6248ed57c1a32a798f22b98dbbd3e7e0e4397c47
adpines <pinesa@pennmedicine.upenn.edu>
2022-01-31 17:14:31 -0500

    viz adapt to fs4

--- scripts/Vis_FaceVec.m
+++ scripts/Vis_FaceVec.m
@@ -22,7 +22,7 @@ function Vis_FaceVec(FaceVecL,FaceVecR,subj,Fn)
 addpath(genpath('/cbica/projects/pinesParcels/multiscale/scripts/derive_parcels/Toolbox'))
 
 %%% Load in surface data
-SubjectsFolder = '/cbica/software/external/freesurfer/centos7/7.2.0/subjects/fsaverage5';
+SubjectsFolder = '/cbica/software/external/freesurfer/centos7/7.2.0/subjects/fsaverage4';
 surfL = [SubjectsFolder '/surf/lh.sphere'];
 surfR = [SubjectsFolder '/surf/rh.sphere'];
 % surface topography
@@ -41,67 +41,53 @@ F_R=faces_r;
 V_R=vx_r;
 
 % now load in medial wall VERTICES
-mw_v_l=read_medial_wall_label([SubjectsFolder '/label/lh.Medial_wall.label']);
-mw_v_r=read_medial_wall_label([SubjectsFolder '/label/rh.Medial_wall.label']);
+%mw_v_l=read_medial_wall_label([SubjectsFolder '/label/lh.Medial_wall.label']);
+%mw_v_r=read_medial_wall_label([SubjectsFolder '/label/rh.Medial_wall.label']);
 
 % all faces that touch a medial wall vertex to be masked
-MW_f1_L=find(ismember(F_L(:,1),mw_v_l));
-MW_f2_L=find(ismember(F_L(:,2),mw_v_l));
-MW_f3_L=find(ismember(F_L(:,3),mw_v_l));
+%MW_f1_L=find(ismember(F_L(:,1),mw_v_l));
+%MW_f2_L=find(ismember(F_L(:,2),mw_v_l));
+%MW_f3_L=find(ismember(F_L(:,3),mw_v_l));
 % rh
-MW_f1_R=find(ismember(F_R(:,1),mw_v_r));
-MW_f2_R=find(ismember(F_R(:,2),mw_v_r));
-MW_f3_R=find(ismember(F_R(:,3),mw_v_r));
+%MW_f1_R=find(ismember(F_R(:,1),mw_v_r));
+%MW_f2_R=find(ismember(F_R(:,2),mw_v_r));
+%MW_f3_R=find(ismember(F_R(:,3),mw_v_r));
 % inclusive - mask if face involves ANY mw vertices
-MW_combined_L=union(MW_f1_L,MW_f2_L);
-MW_combined_L=union(MW_combined_L,MW_f3_L);
+%MW_combined_L=union(MW_f1_L,MW_f2_L);
+%MW_combined_L=union(MW_combined_L,MW_f3_L);
 % now for right hemisphere
-MW_combined_R=union(MW_f1_R,MW_f2_R);
-MW_combined_R=union(MW_combined_R,MW_f3_R);
+%MW_combined_R=union(MW_f1_R,MW_f2_R);
+%MW_combined_R=union(MW_combined_R,MW_f3_R);
 % get inverse for indexing : faces that ARE NOT touching mW verts
-noMW_combined_L=setdiff([1:20480],MW_combined_L);
-noMW_combined_R=setdiff([1:20480],MW_combined_R);
+%noMW_combined_L=setdiff([1:5120],MW_combined_L);
+%noMW_combined_R=setdiff([1:5120],MW_combined_R);
 % further mask derivation
 % MASK WHERE PGG = 0: individ AND group
 % load in GROUP PG
-gLPGfp=['/cbica/projects/pinesParcels/data/princ_gradients/Gradients.lh.fsaverage5.func.gii'];
+gLPGfp=['/cbica/projects/pinesParcels/data/princ_gradients/hcp.gradients_L_3k.func.gii'];
 gLPGf=gifti(gLPGfp);
 gPG_LH=gLPGf.cdata(:,1);
 % right hemi
-gRPGfp=['/cbica/projects/pinesParcels/data/princ_gradients/Gradients.rh.fsaverage5.func.gii'];
+gRPGfp=['/cbica/projects/pinesParcels/data/princ_gradients/hcp.gradients_R_3k.func.gii'];
 gRPGf=gifti(gRPGfp);
 gPG_RH=gRPGf.cdata(:,1);
-% load in subject's PG
-LPGfp=['/cbica/projects/pinesParcels/results/PWs/Proced/' subj '/' subj '_PG_L_10k_rest.func.gii'];
-LPGf=gifti(LPGfp);
-PG_LH=LPGf.cdata(:,1);
-% right hemi
-RPGfp=['/cbica/projects/pinesParcels/results/PWs/Proced/' subj '/' subj '_PG_R_10k_rest.func.gii'];
-RPGf=gifti(RPGfp);
-PG_RH=RPGf.cdata(:,1);
-% calculate PG gradient on sphere
-PGg_L = grad(F_L, V_L, PG_LH);
-PGg_R = grad(F_R, V_R, PG_RH);
 % calculate group PG gradient on sphere
 gPGg_L = grad(F_L, V_L, gPG_LH);
 gPGg_R = grad(F_R, V_R, gPG_RH);
 % get index of where they are 0 in all directions
-PGg_L0=find(all(PGg_L')==0);
 gPGg_L0=find(all(gPGg_L')==0);
-PGg_R0=find(all(PGg_R')==0);
 gPGg_R0=find(all(gPGg_R')==0);
 
 % continue to get unions
-ind_MW_combined_L=union(MW_combined_L,PGg_L0);
-gro_MW_combined_L=union(MW_combined_L,gPGg_L0);
+%gro_MW_combined_L=union(MW_combined_L,gPGg_L0);
 % and right hemi
-ind_MW_combined_R=union(MW_combined_R,PGg_R0);
-gro_MW_combined_R=union(MW_combined_R,gPGg_R0);
+%gro_MW_combined_R=union(MW_combined_R,gPGg_R0);
 % get inverse for indexing : faces that ARE NOT touching mW verts
-i_noMW_combined_L=setdiff([1:20480],ind_MW_combined_L);
-i_noMW_combined_R=setdiff([1:20480],ind_MW_combined_R);
-g_noMW_combined_L=setdiff([1:20480],gro_MW_combined_L);
-g_noMW_combined_R=setdiff([1:20480],gro_MW_combined_R);
+%g_noMW_combined_L=setdiff([1:5120],gro_MW_combined_L);
+%g_noMW_combined_R=setdiff([1:5120],gro_MW_combined_R);
+%%%%% version without MW mask explicitly
+g_noMW_combined_L=setdiff([1:5120],gPGg_L0);
+g_noMW_combined_R=setdiff([1:5120],gPGg_R0);
 
 
 %%% PCA section
@@ -125,18 +111,18 @@ g_noMW_combined_R=setdiff([1:20480],gro_MW_combined_R);
 %%%%%%%%
 
 
-data=zeros(1,20480);
+data=zeros(1,5120);
 data(g_noMW_combined_L)=FaceVecL;
 
 % fixed colorscale for correlations
-mincol=0;
-maxcol=.33;
+mincol=-.2;
+maxcol=.2;
 
-custommap=colormap('inferno');
+%custommap=colormap('inferno');
 % for red/blue 0-centered
-%custommap=colormap(b2r(mincol,maxcol));
+custommap=colormap(b2r(mincol,maxcol));
 
-[vertices, faces] = freesurfer_read_surf('/cbica/software/external/freesurfer/scientificlinux6/6.0.0/subjects/fsaverage5/surf/lh.inflated');
+[vertices, faces] = freesurfer_read_surf('/cbica/software/external/freesurfer/scientificlinux6/6.0.0/subjects/fsaverage4/surf/lh.inflated');
 
 asub = subaxis(2,2,1, 'sh', 0, 'sv', 0, 'padding', 0, 'margin', 0);
 
@@ -151,6 +137,7 @@ shading flat;
 camlight;
 alpha(1)
 
+length(faces)
 
 set(gca,'CLim',[mincol,maxcol]);
 set(aplot,'FaceColor','flat','FaceVertexCData',data','CDataMapping','scaled');
@@ -178,10 +165,10 @@ set(aplot,'FaceColor','flat','FaceVertexCData',data','CDataMapping','scaled');
 
 
 %%% right hemisphere
-data=zeros(1,20480);
+data=zeros(1,5120);
 data(g_noMW_combined_R)=FaceVecR;
 
-[vertices, faces] = freesurfer_read_surf('/cbica/software/external/freesurfer/scientificlinux6/6.0.0/subjects/fsaverage5/surf/rh.inflated');
+[vertices, faces] = freesurfer_read_surf('/cbica/software/external/freesurfer/scientificlinux6/6.0.0/subjects/fsaverage4/surf/rh.inflated');
 
 asub = subaxis(2,2,2, 'sh', 0.0, 'sv', 0.0, 'padding', 0, 'margin', 0,'Holdaxis');
 aplot = trisurf(faces, vertices(:,1), vertices(:,2), vertices(:,3),data)
---
+++ scripts/directionalDistribViz.py
@@ -0,0 +1,45 @@
+# from local
+import numpy as np
+from scipy import special as special
+from scipy.optimize import minimize as sp_min
+import matplotlib.pyplot as pl
+from scipy.io import loadmat
+
+######
+
+# Subj Data
+testSubj_L=loadmat('C:/Users/adam/testSubjMat_L.mat', squeeze_me=True)
+testSubj_R=loadmat('C:/Users/adam/testSubjMat_R.mat', squeeze_me=True)
+
+# COL 0 = PROP. OF TRs THAT ARE BU
+# COL 1 = BU VECTOR HORIZONTAL COMPONENT
+# COL 2 = BU VECTOR VERTICAL COMPONENT
+# COL 3 = TD VECTOR HORIZONTAL COMPONENT - fucked
+# COL 4 = TD VECTOR VERTICAL COMPONENT - fucked
+# COL 5 = BU VECTOR LENGTH
+# COL 6 = TD VECTOR LENGTH - fucked
+
+LeftHemi=testSubj_L['OutDf_L']
+RightHemi=testSubj_R['OutDf_R']
+
+# plot out normative BU and TD vector
+#pl.scatter([LeftHemi[400,1],LeftHemi[400,3]],[LeftHemi[400,2],LeftHemi[400,4]])
+#pl.xlim([-1,1])
+#pl.ylim([-1,1])
+#pl.show()
+
+# scatter plot of resultant length of vectors: seems to indicate strong TD exitsts where strong TD exists
+pl.scatter(LeftHemi[1:1500,3],LeftHemi[1:1500,4])
+pl.show()
+
+np.corrcoef(LeftHemi[1:1500,3].astype(float),LeftHemi[1:1500,4].astype(float))
+# Normative PGG directions
+
+# calculate distance in radians so as to produce 2pi of variation
+
+# convert input data to radians
+
+# will need mask indiceshttps://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.vonmises.html as well
+
+#####
+
