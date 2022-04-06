dc45df0c2a35622c45ef68ce6c6ce1b77c22ad5f
adpines <pinesa@pennmedicine.upenn.edu>
2022-04-06 13:37:10 -0400

    gro dmn quantification

--- scripts/AggFC.m
+++ scripts/AggFC.m
@@ -2,8 +2,8 @@
 addpath(genpath('/cbica/projects/hcpd/scripts/tools'));
 % read in subj list
 Subjs=readtable('~/PWs/rs_subs.csv');
-%initialize output array (one row for subj name, one for dmn segreg
-DMNseg=cell(height(Subjs),2);
+%initialize output array (one row for subj name, one for dmn segreg in individualized nets, one in group
+DMNseg=cell(height(Subjs),3);
 % set parent FP for shorter FPs in loop
 parentfp = '/cbica/projects/hcpd/data/motMasked_contSegs/';
 sspParentfp='/cbica/projects/hcpd/data/SSPs/SingleParcel_1by1_fslr/';
@@ -86,7 +86,55 @@ for s=1:height(Subjs)
 	notDMvec=Kvec(Kvec~=11);
 	Dseg=mean(FC(11,notDMvec))
 	DMNseg(s,1)=cellstr(subj);
-	DMNseg(s,2)=num2cell(Dseg);	
+	DMNseg(s,2)=num2cell(Dseg);
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	%%%%%% now take measurements from group map %%%%%
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+	% this is inefficient to do every loop, but is easier to gaurantee equivalent masking
+	% initialize network partition-holder
+	GPartFrame=zeros((length(nomw_mask_l)+length(nomw_mask_r)),17);
+	% load in network partition
+	for k=1:17
+	        GFPL=['/cbica/projects/hcpd/data/SSPs/SingleParcel_1by1_fslr/Group/resamp_3k_network_L' num2str(k) '.func.gii'];
+	        GFPR=['/cbica/projects/hcpd/data/SSPs/SingleParcel_1by1_fslr/Group/resamp_3k_network_R' num2str(k) '.func.gii'];
+	        V_Topog_L=gifti(GFPL);
+	        V_Topog_R=gifti(GFPR);
+	        % extract loadings from file, plant into vertices by networks matrix for this subject
+	        GPartFrame(1:length(nomw_mask_l),k)=V_Topog_L.cdata(nomw_mask_l,1);
+	        GPartFrame((length(nomw_mask_l)+1):(length(nomw_mask_l)+length(nomw_mask_r)),k)=V_Topog_R.cdata(nomw_mask_r,1);
+	end
+	% take max of each vertex to get hard parcel
+	[ ~ , GhardParcel]=max(GPartFrame,[],2);
+
+	% initialize FCmat
+        GFC=zeros(17,17);
+        for k=1:17
+                % get vertices in this network
+                VertsInNet=find(GhardParcel==k);
+                % index out within network connectivity
+                curNetMat=conmat(VertsInNet,VertsInNet);
+                % get within-network connectivity: using TRIU to get a mask of the upper triangle and avoid the diagonal/redundancy
+                wincon=mean(curNetMat(find(~triu(ones(size(curNetMat))))));
+                GFC(k,k)=wincon;
+                % make vector for all values except for current K (N) to loop through
+                Kvec=1:17;
+                NotKvec=Kvec(Kvec~=k);
+                % one fewer networks than number of networks, as we are looping over OTHER networks (other than k)
+                for b=1:16
+                                curOtherNet=NotKvec(b);
+                                % index vertices not in up-one-level-network-N loop
+                                NotKind=find(GhardParcel==curOtherNet);
+                                bwMat=conmat(VertsInNet,NotKind);
+                                bwcon=mean(mean(bwMat));
+                                % plop it into 17x17 matrix
+                                GFC(k,curOtherNet)=bwcon;
+                end
+        end
+	notDMvec=Kvec(Kvec~=11);
+        DsegGro=mean(GFC(11,notDMvec))
+	% and plop group-map based into df
+	DMNseg(s,3)=num2cell(DsegGro);
 end
 % save out aggregate dmn 
 writetable(table(DMNseg),'~/results/DMNseg.csv')
--- scripts/archive/ExtractNetComponents.m
+++ scripts/archive/ExtractNetComponents.m
@@ -2,7 +2,7 @@ addpath(genpath('~/dropbox/cifti-matlab/'));
 % use 600 TRs plus subj list
 subjs=readtable('/cbica/projects/pinesParcels/PWs/G600TRs.txt','ReadVariableNames',false);
 % read in hard parcel to replace Cort data with
-HP=cifti_read('/cbica/projects/pinesParcels/data/princ_gradients/hcp.gradients.dscalar.nii');
+HP=cifti_read('/cbica/projects/pinesParcels/data/hcp.gradients.dscalar.nii');
 
 % loop over each subj
 for s=1:height(subjs)
@@ -20,3 +20,15 @@ for s=1:height(subjs)
 	end
 	toc
 end
+
+% and once for the group map (which subject doesn't matter, using last from loop)
+Subjfp=['/cbica/projects/hcpd/data/SSPs/SingleParcel_1by1_fslr/' subj];
+Loading_Mat = load([Subjfp '/IndividualParcel_Final_sbj1_comp17_alphaS21_1_alphaL300_vxInfo1_ard0_eta0/init_UV.mat']);
+Loading_Mat = cell2mat(Loading_Mat.V);
+% for all 17 networks
+for i = 1:17
+        HP.cdata(1:59412)=Loading_Mat(:,i);
+        outputfile=['/cbica/projects/hcpd/data/SSPs/SingleParcel_1by1_fslr/Group/AtlasLoading_Network_' num2str(i) '.dscalar.nii'];
+	cifti_write(HP,outputfile)
+end
+
