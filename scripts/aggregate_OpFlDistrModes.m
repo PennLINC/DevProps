51c02d944d9ca14617aca8edb0221d69d1ce88a5
adpines <pinesa@pennmedicine.upenn.edu>
2022-05-09 09:43:37 -0400

    aggreg_opFlModes streamlined to just td prop

--- scripts/aggregate_OpFlDistrModes.m
+++ scripts/aggregate_OpFlDistrModes.m
@@ -44,33 +44,33 @@ for s = 1:height(Subjs)
 		BUTD_R=cell2mat(BUTD_R.OutDf_R);
 		%discretize each face's distribution
 		for f = 1:5120
-			binnedFaceDirs=histcounts(AngsL(:,f),edges);
-        		[M,I]=max(binnedFaceDirs);
-        		IntermedArray_L(f,s)=I;
-        		%%% find prominence from non-adjacent modes
-        		% make a temp. binnedFaceDirs vector with padding on sides: will allow for uniform neighbor removal
-        		tmpBFDvec=zeros(1,22);
-        		tmpBFDvec(3:20)=binnedFaceDirs;
-        		% I and I+4 instead of I-2 I+2 because we added 2 0s to the start of the vector (-2 -> 0,+2 -> 4)
-        		tmpBFDvec((I):(I+4))=[];
-        		% get updated binnedFaceDirs: those non-adjacent
-        		[M2,I2]=max(tmpBFDvec);
-       			 % get relative prominence of secondary peak
-        		ModesL_Prom(f,s)=1-(M2/M);
-        		% and for right
-        		binnedFaceDirs=histcounts(AngsR(:,f),edges);
-        		[M,I]=max(binnedFaceDirs);
-        		IntermedArray_R(f,s)=I;
-        		%%% find prominence from non-adjacent modes
-        		% make a temp. binnedFaceDirs vector with padding on sides: will allow for uniform neighbor removal
-        		tmpBFDvec=zeros(1,22);
-        		tmpBFDvec(3:20)=binnedFaceDirs;
-        		% I and I+4 instead of I-2 I+2 because we added 2 0s to the start of the vector (-2 -> 0,+2 -> 4)
-        		tmpBFDvec((I):(I+4))=[];
-        		% get updated binnedFaceDirs: those non-adjacent
-        		[M2,I2]=max(tmpBFDvec);
-        		% get relative prominence of secondary peak
-        		ModesR_Prom(f,s)=1-(M2/M);
+		%	binnedFaceDirs=histcounts(AngsL(:,f),edges);
+        	%	[M,I]=max(binnedFaceDirs);
+        	%	IntermedArray_L(f,s)=I;
+        	%	%%% find prominence from non-adjacent modes
+        	%	% make a temp. binnedFaceDirs vector with padding on sides: will allow for uniform neighbor removal
+        	%	tmpBFDvec=zeros(1,22);
+        	%	tmpBFDvec(3:20)=binnedFaceDirs;
+        	%	% I and I+4 instead of I-2 I+2 because we added 2 0s to the start of the vector (-2 -> 0,+2 -> 4)
+        	%	tmpBFDvec((I):(I+4))=[];
+        	%	% get updated binnedFaceDirs: those non-adjacent
+        	%	[M2,I2]=max(tmpBFDvec);
+       		%	 % get relative prominence of secondary peak
+        	%	ModesL_Prom(f,s)=1-(M2/M);
+        	%	% and for right
+        	%	binnedFaceDirs=histcounts(AngsR(:,f),edges);
+        	%	[M,I]=max(binnedFaceDirs);
+        	%	IntermedArray_R(f,s)=I;
+        	%	%%% find prominence from non-adjacent modes
+        	%	% make a temp. binnedFaceDirs vector with padding on sides: will allow for uniform neighbor removal
+        	%	tmpBFDvec=zeros(1,22);
+        	%	tmpBFDvec(3:20)=binnedFaceDirs;
+        	%	% I and I+4 instead of I-2 I+2 because we added 2 0s to the start of the vector (-2 -> 0,+2 -> 4)
+        	%	tmpBFDvec((I):(I+4))=[];
+        	%	% get updated binnedFaceDirs: those non-adjacent
+        	%	[M2,I2]=max(tmpBFDvec);
+        	%	% get relative prominence of secondary peak
+        	%	ModesR_Prom(f,s)=1-(M2/M);
 			% insert prop. TDm note contingencies. BUTD file is already masked
 			if f < 4590;
 				TDP_mL(f,s)=1-BUTD_L(f,1);
@@ -91,11 +91,14 @@ ModesL_Prom=ModesL_Prom(:,PopulatedColsL);
 ModesR_Prom=ModesR_Prom(:,PopulatedColsR);
 
 %get modes across subjs, avg. prominence
+
+%%% updated to just top-down proportion
+
 for f = 1:5120
-	OutArray_L(f,1)=mode(IntermedArray_L(f,:));
-	OutArray_R(f,1)=mode(IntermedArray_R(f,:));
-	GroPromArray_L(f,1)=mean(ModesL_Prom(f,:));
-	GroPromArray_R(f,1)=mean(ModesR_Prom(f,:));
+	%OutArray_L(f,1)=mode(IntermedArray_L(f,:));
+	%OutArray_R(f,1)=mode(IntermedArray_R(f,:));
+	%GroPromArray_L(f,1)=mean(ModesL_Prom(f,:));
+	%GroPromArray_R(f,1)=mean(ModesR_Prom(f,:));
 	% same contingencies as above
 	if f < 4590;
 		TDP_vL(f,1)=mean(TDP_mL(f,:));
@@ -108,18 +111,18 @@ for f = 1:5120
 end
 
 % read magnitude length vecs for plot masking
-magL=readtable('~/data/vHL.csv');
-magR=readtable('~/data/vHR.csv');
+%magL=readtable('~/data/vHL.csv');
+%magR=readtable('~/data/vHR.csv');
 
 %save
-writetable(table(OutArray_L),'/cbica/projects/pinesParcels/results/PWs/ModeModes4_L.csv');
-writetable(table(OutArray_R),'/cbica/projects/pinesParcels/results/PWs/ModeModes4_R.csv');
+%writetable(table(OutArray_L),'/cbica/projects/pinesParcels/results/PWs/ModeModes4_L.csv');
+%writetable(table(OutArray_R),'/cbica/projects/pinesParcels/results/PWs/ModeModes4_R.csv');
 
-Vis_FaceVec_modes(OutArray_L,OutArray_R,'GroupPGGModes',GroPromArray_L,GroPromArray_R)
+%Vis_FaceVec_modes(OutArray_L,OutArray_R,'GroupPGGModes',GroPromArray_L,GroPromArray_R)
 
 % filling in unmasked faces with unmasked BU Prop averages, which are the first 45xx
 Vis_FaceVec_Bup(TDP_vL(1:4589),TDP_vR(1:4595),'GroupTDProp.png')
-Vis_FaceVec_Bup_desat(TDP_vL(1:4589),TDP_vR(1:4595),'GroupTDProp_desat.png',table2array(magL),table2array(magR))
+%Vis_FaceVec_Bup_desat(TDP_vL(1:4589),TDP_vR(1:4595),'GroupTDProp_desat.png',table2array(magL),table2array(magR))
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -137,37 +140,37 @@ faceModesL_Prom=zeros(1,5120);
 faceModesR_Prom=zeros(1,5120);
 %discretize each face's distribution
 for f = 1:5120
-	%%%% find mode and prominence
-	binnedFaceDirs=histcounts(AngsL(:,f),edges);	
-	[M,I]=max(binnedFaceDirs);
-	sOutArray_L(f)=I;
-	%%% find prominence from non-adjacent modes
-	% make a temp. binnedFaceDirs vector with padding on sides: will allow for uniform neighbor removal
-	tmpBFDvec=zeros(1,22);
-	tmpBFDvec(3:20)=binnedFaceDirs;
-	% I and I+4 instead of I-2 I+2 because we added 2 0s to the start of the vector (-2 -> 0,+2 -> 4)
-	tmpBFDvec((I):(I+4))=[];
-	% get updated binnedFaceDirs: those non-adjacent
-	[M2,I2]=max(tmpBFDvec);
-	% get relative prominence of secondary peak
-	faceModesL_Prom(f)=1-(M2/M);
-	% and for right
-        binnedFaceDirs=histcounts(AngsR(:,f),edges);
-        [M,I]=max(binnedFaceDirs);
-        sOutArray_R(f)=I;
-        %%% find prominence from non-adjacent modes
-        % make a temp. binnedFaceDirs vector with padding on sides: will allow for uniform neighbor removal
-        tmpBFDvec=zeros(1,22);
-        tmpBFDvec(3:20)=binnedFaceDirs;
-        % I and I+4 instead of I-2 I+2 because we added 2 0s to the start of the vector (-2 -> 0,+2 -> 4)
-        tmpBFDvec((I):(I+4))=[];
-        % get updated binnedFaceDirs: those non-adjacent
-        [M2,I2]=max(tmpBFDvec);
-        % get relative prominence of secondary peak
-        faceModesR_Prom(f)=1-(M2/M);
-end
+%	%%%% find mode and prominence
+%	binnedFaceDirs=histcounts(AngsL(:,f),edges);	
+%	[M,I]=max(binnedFaceDirs);
+%	sOutArray_L(f)=I;
+%	%%% find prominence from non-adjacent modes
+%	% make a temp. binnedFaceDirs vector with padding on sides: will allow for uniform neighbor removal
+%	tmpBFDvec=zeros(1,22);
+%	tmpBFDvec(3:20)=binnedFaceDirs;
+%	% I and I+4 instead of I-2 I+2 because we added 2 0s to the start of the vector (-2 -> 0,+2 -> 4)
+%	%tmpBFDvec((I):(I+4))=[];
+%	% get updated binnedFaceDirs: those non-adjacent
+%	[M2,I2]=max(tmpBFDvec);
+%	% get relative prominence of secondary peak
+%	faceModesL_Prom(f)=1-(M2/M);
+%	% and for right
+%       binnedFaceDirs=histcounts(AngsR(:,f),edges);
+%        [M,I]=max(binnedFaceDirs);
+%        sOutArray_R(f)=I;
+%        %%% find prominence from non-adjacent modes
+%        % make a temp. binnedFaceDirs vector with padding on sides: will allow for uniform neighbor removal
+%        tmpBFDvec=zeros(1,22);
+%        tmpBFDvec(3:20)=binnedFaceDirs;
+%        % I and I+4 instead of I-2 I+2 because we added 2 0s to the start of the vector (-2 -> 0,+2 -> 4)
+%        tmpBFDvec((I):(I+4))=[];
+%        % get updated binnedFaceDirs: those non-adjacent
+%        [M2,I2]=max(tmpBFDvec);
+%        % get relative prominence of secondary peak
+%        faceModesR_Prom(f)=1-(M2/M);
+%end
 
-Vis_FaceVec_modes(sOutArray_L,sOutArray_R,'fn2_4.png',faceModesL_Prom,faceModesR_Prom)
+%Vis_FaceVec_modes(sOutArray_L,sOutArray_R,'fn2_4.png',faceModesL_Prom,faceModesR_Prom)
 
 % and just include BUprop plotting so its all in the same place for single subj instances
 
