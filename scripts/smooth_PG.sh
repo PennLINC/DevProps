4d1d4b95ce2e934fbc0bc822c1fef2d0e0b0d3c8
adpines <pinesa@pennmedicine.upenn.edu>
2022-05-15 09:36:35 -0400

    Delete smooth_PG.sh

--- scripts/smooth_PG.sh
+++
@@ -1,3 +0,0 @@
-# converting 15 mm FWHM to sigma: https://brainder.org/2011/08/20/gaussian-kernels-convert-fwhm-to-sigma/
-wb_command -metric-smoothing ~/dropbox/fsaverage5_lh.pial_avg.surf.gii hcp.gradients_L_10k.func.gii 6.37 hcpd_gradients_L_10k_15smooth.func.gii
-wb_command -metric-smoothing ~/dropbox/fsaverage5_rh.pial_avg.surf.gii hcp.gradients_R_10k.func.gii 6.37 hcpd_gradients_R_10k_15smooth.func.gii
